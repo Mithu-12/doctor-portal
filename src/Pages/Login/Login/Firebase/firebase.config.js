66292a683222933ca97b0139194ac5ca0eead140
Mithu-12 <mithuvowmick96@gmail.com>
2022-02-16 03:58:54 -0600

    last

--- src/Pages/Login/Login/Firebase/firebase.config.js
+++ src/Pages/Login/Login/Firebase/firebase.config.js
@@ -1,9 +1,20 @@
 const firebaseConfig = {
-    apiKey: process.env.REACT_APP_API_KEY,
-    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-    projectId: process.env.REACT_APP_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-    appId: process.env.REACT_APP_APP_ID,
-};
+    apiKey: "AIzaSyAVhj8m_yOseFUnYrPPseTgc1o7r6n6I5c",
+    authDomain: "jobhunt-4b257.firebaseapp.com",
+    projectId: "jobhunt-4b257",
+    storageBucket: "jobhunt-4b257.appspot.com",
+    messagingSenderId: "776184469191",
+    appId: "1:776184469191:web:bad0f1567d26de286690e2",
+    measurementId: "G-LT36C0MWGN"
+  };
+
+
+// const firebaseConfig = {
+//     apiKey: process.env.REACT_APP_API_KEY,
+//     authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+//     projectId: process.env.REACT_APP_PROJECT_ID,
+//     storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+//     messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+//     appId: process.env.REACT_APP_APP_ID,
+// };
 export default firebaseConfig;
